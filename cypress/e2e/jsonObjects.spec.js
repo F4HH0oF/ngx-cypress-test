0e10af120407ceb131a102afa469e4097f9e23a3
F4HH0oF <alekzanderignatov@gmail.com>
2023-05-07 11:05:27 +0300

    JSON Objects

---
+++ cypress/e2e/jsonObjects.spec.js
@@ -0,0 +1,10 @@
+const { createYield } = require("typescript");
+
+describe("JSON Objects", () => {
+  it("JSON Objects", () => {
+    cy.openHomePage();
+
+    const simpleObj = { key: "value", key2: "value2" };
+    const simpleArr = ["one", "two", "three"];
+  });
+});
